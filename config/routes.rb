1c7973b5ff1986102eb0a4e903600c65009e839e
Parham Javadi <pjfooeve09@yahoo.com>
2021-04-14 12:17:01 -0700

    commit tree 3c96c387ba39a05c97f558cb4153cd13c039347d

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'about', to: 'static#about' # 'static#about' is called the controller action. If the name is #about, then the view file name needs to be about.html.erb if it's for implicit viewing. the "def about" has nothing to do with the naming convention. If the word here is "static", then controller needs to be StaticController, or if the name is "hello" then it needs to be HelloController
 end
